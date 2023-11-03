e9f7a84dc3a17a6b2b51bd311bd2f6d1ce45fc10
tynan Sampsel <tynansampselr@gmail.com>
2023-11-03 17:19:21 -0300

    v1.2 added geolocation from google.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import { useState, useEffect } from "react";
 import HourlyWeatherCard from './HourlyWeatherCard';
 import { Link } from 'react-router-dom';
+import {
+  setKey,
+  setDefaults,
+  setLanguage,
+  setRegion,
+  fromAddress,
+  fromLatLng,
+  fromPlaceId,
+  setLocationType,
+  geocode,
+  RequestType,
+} from "react-geocode";
+
+setDefaults({
+  key: "AIzaSyCVnL-qU-zuU09BYnrpCKxNCo6smVe2mw0", // Your API key here.
+  language: "en", // Default language for responses.
+  region: "es", // Default region for responses.
+});
 
 function App() {
 
   const [hour, setHour] = useState([]);
+  const [cityName, setcityName] = useState("");
   const [temperature, setTemperature] = useState([]);
   const [humidity, setHumidity] = useState([]);
   const [windSpeed, setWindSpeed] = useState([]);
@@ -13,12 +32,27 @@ function App() {
 
   useEffect(() => {
     GetWeather();
+    GetCityName();
   }, [])
+
+
+  const GetCityName = () => {
+    
+  }
 
   const GetWeather = () => {
     navigator.geolocation.getCurrentPosition(function (position) {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
+
+      fromLatLng(latitude, longitude)
+      .then(({ results }) => {
+        const { lat, lng } = results[0].geometry.location;
+        setcityName(results[0].address_components[1].short_name)
+        console.log(results[0].address_components[1].short_name)
+        console.log(results);
+      })
+      .catch(console.error);
 
       fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,windspeed_10m&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`)
         .then(res => res.json())
@@ -40,7 +74,7 @@ function App() {
     <div className="App">
       <form>
       </form>
-      <h1 className="cityName">Halifax</h1>
+      <h1 className="cityName">{cityName != null ? cityName : ". . ."}</h1>
       <div className="lineTop"></div>
       <div className="carouselWrapper">
         <div className="carousel"  >
